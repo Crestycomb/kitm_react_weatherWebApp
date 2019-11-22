3010b7a0c617fbc20cfe808a38a7a321d6419be8
Crestycomb <ignas.klydzia@gmail.com>
2019-11-22 17:41:25 +0200

    rendering all the small weather cards

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -25,88 +25,89 @@ function Square(props) {
 class ApiStuff extends Component {
     constructor() {
         super();
+
         this.state = {
-            data: {},
+            currentLocation: "kaunas/",
             hasErrors: null,
             isLoaded: false,
-            planets: {},
             randomP: <h2> thingy</h2>
+
         };
     }
 
     cors = "https://cors-anywhere.herokuapp.com/";
     startPoint = "https://api.meteo.lt/v1/";
     places = "places/";
-    currentLocation = "vilnius/";
+
     forecasts = "forecasts/";
     forecastType = "long-term/";
 
     /*
         stuff to change the forecast place would go here
     */
-/*
-    working example
-    componentDidMount() {
-        fetch("https://swapi.co/api/planets/4/")
-            .then(res => res.json())
-            .then(res => this.setState({ planets: res }))
-            .catch(() => this.setState({ hasErrors: true }));
 
-
-    }
-*/
 
     componentDidMount() {
-        let endpoint = this.cors + this.startPoint + this.places + this.currentLocation + this.forecasts + this.forecastType;
+        let endpoint = this.cors + this.startPoint + this.places + this.state.currentLocation + this.forecasts + this.forecastType;
         fetch(endpoint)
             .then(res => res.json())
-            .then(res => this.setState({ data: res }))
-            .then(randomP => this.setState({randomP: this.state.data.place.code }))
-            .catch(() => this.setState({ hasErrors: true }));
+            .then(res => this.setState({data: res}))
+            .then(randomP => this.setState({randomP: this.state.data.place.code}))
+            .catch(() => this.setState({hasErrors: true}));
     }
 
-
-
-/*
-    componentDidMount() {
-        let endpoint = this.cors + this.startPoint + this.places + this.currentLocation + this.forecasts + this.forecastType;
-        fetch(endpoint)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        data: result.data
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-*/
-
-    renderWeatherCard(i) {
-        return <Square
-
-
-        />;
-    }
 
     render() {
         return (
-            <div className="col">
-                <p>{this.state.data.forecastType}</p>
-                <p>{this.state.randomP}</p>
-            </div>
 
+
+            <div className="col">
+
+                <p>ayy lmao</p>
+
+                {/*checks if data is fetched. only renders if it is.*/}
+                {this.state && this.state.data &&
+                <>
+
+
+                    {this.renderWeatherCard(this.state.data.forecastTimestamps[1])}
+
+                    {/*renders all the small cards*/}
+                    {this.renderWeatherCardList(this.state.data.forecastTimestamps)}
+
+                    <p className="clearfix">.</p>
+                    <p>{this.state.data.forecastType}</p>
+                    <p>{this.state.data.place.code}</p>
+                    <p>{this.state.data.place.coordinates.latitude}</p>
+                    <p>{this.state.data.forecastTimestamps[1].windSpeed}</p>
+                </>
+                }
+            </div>
         )
+    }
+
+    renderWeatherCardList(forecasts) {
+        return (
+            <div>
+                {
+                    forecasts.map((forecast) =>
+                        <>{this.renderWeatherCard(forecast)}</>
+                    )
+                }
+            </div>
+        );
+    }
+
+    renderWeatherCard(forecast) {
+        return (
+            <div className="bg-info w-25 float-left p-2 m-4">
+                <p>time: {forecast.forecastTimeUtc}</p>
+                <p>temp: {forecast.airTemperature}</p>
+                <p>precip: {forecast.totalPrecipitation}</p>
+                <p>direction: {forecast.windDirection}</p>
+
+            </div>
+        );
     }
 }
 
